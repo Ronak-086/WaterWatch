776ed8ef0a6c4bc21a79a7ac705043824d139248
Nithin-2005-dev <nithinkumar2005b@gmail.com>
2025-06-10 08:22:28 +0530

    vercel error fixing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
    server: {
     proxy: {
       '/api': {
-        target: 'https://water-watch-si4e.vercel.app/',
+        target: 'https://water-watch-si4e.vercel.app',
         changeOrigin: true,
         secure: false,
       },
